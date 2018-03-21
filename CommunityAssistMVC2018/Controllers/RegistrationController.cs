dfb0ffa66936d9a6d6f56c87103cb309d67e7fae
Bennett <Bkielbon@gmail.com>
2018-03-21 03:56:41 -0700

    Registration Commit

---
+++ CommunityAssistMVC2018/Controllers/RegistrationController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssistMVC2018.Models;
+
+namespace CommunityAssistMVC2018.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+
+        // GET: Registration
+        public ActionResult Index()
+        {
+            return View(db.People.ToList());
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register([Bind(Include = "LastName, FirstName, Email, PlanPassword, Apartment, Street, City, State, Zipcode, Phone")] NewPerson p)
+        {
+            int result = db.usp_Register(p.LastName, p.FirstName, p.Email,p.PlainPassword,p.Apartment,p.Street,p.City,p.State,p.Zipcode,p.Phone);
+            if(result !=-1)
+            {
+                return RedirectToAction("ThankYou");
+            }
+
+            return View(p);
+        }
+
+
+    }
+}
---
+++ CommunityAssistMVC2018/Models/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CommunityAssistMVC2018.Models
+{
+    public class Message
+    {
+        public string MessageText { get; set; }
+
+    }
+}

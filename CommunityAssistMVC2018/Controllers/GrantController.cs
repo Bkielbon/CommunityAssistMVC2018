7692b970f6ef650aa2dd78088e6183676114df4a
Bennett <Bkielbon@gmail.com>
2018-03-22 14:52:22 -0700

    Grant Application for Assistance commit

---
+++ CommunityAssistMVC2018/Controllers/GrantController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityAssistMVC2018.Models;
+
+namespace CommunityAssistMVC2018.Controllers
+{
+    public class GrantController : Controller
+    {
+        CommunityAssist2017Entities db = new CommunityAssist2017Entities();
+
+        // GET: Grant
+        public ActionResult Index()
+
+        {
+            if (Session["userKey"] == null)
+            {
+                Message msg = new Message("Must be logged in to apply for a grant!");
+                return RedirectToAction("Result", msg);
+
+            }
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "GrantType, PersonKey , GrantApplicationDate, GrantApplicationReason, GrantApplicationRequestAmount  ")]GrantApplication g)
+        {
+
+            g.PersonKey = (int)Session["userkey"];
+            g.GrantApplicationStatusKey = 1;
+            g.GrantAppicationDate = DateTime.Now;
+            db.GrantApplications.Add(g);
+            db.SaveChanges(); 
+
+
+            Message msg = new Message("Thank you for the applying for a grant!");
+            return View("Result", msg);
+
+        }
+        public ActionResult Result(Message msg)
+        {
+            return View(msg);
+        }
+    }
+}
